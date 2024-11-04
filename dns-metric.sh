8ee51b0294e18c483ef2bfd6a5e174b03b95f908
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2024-11-04 13:02:36 -0300

    feat: domains.txt

--- dns-metric.sh
+++ dns-metric.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-for (( c=1; c<=5; c++))
-do
+#for (( c=1; c<=5; c++)) do
+while read arg; do
 sudo killall -USR2 systemd-resolved
 sudo resolvectl flush-caches
 sudo killall -USR1 systemd-resolved
-if [ "$#" -ne 1 ]; then
-    echo "Use: $0 <domain>"
-    exit 1
-fi
+#if [ "$#" -ne 1 ]; then
+ #   echo "Use: $0 <domain>"
+    #exit 1
+#fi
 
 if dpkg-query -W -f='${Status}' dnsutils 2>/dev/null | grep -q "install ok installed"; then
     echo "dig installed"
@@ -16,15 +16,15 @@ else
     sudo apt install dnsutils
 fi
 
-DOMAIN=$1
+DOMAIN=$arg
 
 resolve=$(dig +stats "$DOMAIN" | grep "Query time" | awk '{print $4}')
 
 tcp=$(curl -s -o /dev/null -w "dns:%{time_namelookup}, redir:%{time_redirect}, tcp:%{time_connect}\n" "$DOMAIN")
 #sleep 30
 #printf "%s" "teste"
-printf "%s" "{id: $c, resolve: $resolve, tcp: $tcp}"
+printf "%s" "{domain: $DOMAIN, dns-time: $resolve, tcp: $tcp}"
 touch teste.txt
-echo "{id: $c, resolve: $resolve, tcp: $tcp}" >> teste.txt
+echo "{domain: $DOMAIN, dns-time: $resolve, tcp: $tcp}" >> teste.txt
 done
 
